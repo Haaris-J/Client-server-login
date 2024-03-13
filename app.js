b0a70d0949c2b2ca93d862a78ca4776f37bf0df6
muhammadhaarisj <muhammadhaaris.j@gmail.com>
2024-03-13 15:09:16 -0400

    removed index.html

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 
 // Default page route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
 // route and logic for register
