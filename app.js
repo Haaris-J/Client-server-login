fdb66844aed45457ae830ad9aaf236c5738cf315
muhammadhaarisj <muhammadhaaris.j@gmail.com>
2024-03-13 13:17:50 -0400

    initiated express() and set use

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
