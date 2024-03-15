c7525ef15b676c30ee15a62188bd858fd7906d16
muhammadhaarisj <muhammadhaaris.j@gmail.com>
2024-03-15 03:00:17 -0400

    Added login logic

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // author1:muhammadhaaris.j
-// author2:
 
 // importing modules
 const express = require('express');
@@ -19,20 +18,32 @@ const PORT = process.env.PORT || 3000;
 
 // Default page route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // route and logic for register
-app.post('/register', async(req, res) => {
+app.post('/register', async (req, res) => {
     const { username, secretcode, password, email, answer1, answer2 } = req.body;
-    console.log(`${username} ${secretcode} ${password} ${email} ${answer1} ${answer2}`);
+    let userExist = false;
     try {
-        const hashedsecretcode = await bcrypt.hash(secretcode, 1);
-        console.log(`${hashedsecretcode}`);
-        const hashedPassword = await bcrypt.hash(password, 1);
-        console.log(`${hashedPassword}`);
-        await data.create({ username, secretcode: hashedsecretcode, password: hashedPassword, email, answer1, answer2 });
-        res.sendFile(path.join(__dirname, 'public', 'login.html'));
+        await data.findOne({
+            username: `${username}`,
+        })
+            .then((document) => {
+                if (document != null) {
+                    userExist = true;
+                }
+            });
+        console.log(userExist);
+        if (!userExist) {
+            const hashedsecretcode = await bcrypt.hash(secretcode, 1);
+            const hashedPassword = await bcrypt.hash(password, 1);
+            await data.create({ username, secretcode: hashedsecretcode, password: hashedPassword, email, answer1, answer2 });
+            res.sendFile(path.join(__dirname, 'public', 'login.html'));
+        } else {
+            res.status(427).send(`<h1 style="color:red;">User Name already exist!</h1><br><a href="register.html">Return to Register page</a>`);
+        }
+
     } catch (error) {
         console.log(error)
         res.status(500).send(`<h1 style="color:red;">Registration failed!</h1><br><a href="http://localhost:${PORT}/">Return to website</a>`);
@@ -40,7 +51,36 @@ app.post('/register', async(req, res) => {
 });
 
 // route and logic for login
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
+    const { username, secretcode, password } = req.body;
+    let userExist = false;
+    let dbrecord = {};
+    try {
+        await data.findOne({
+            username: `${username}`,
+        })
+            .then((document) => {
+                if (document != null) {
+                    userExist = true;
+                    dbrecord = document;
+                }
+            });
+        if(userExist){
+            console.log('Username valid');
+            if(await bcrypt.compare(password, dbrecord.password) && await bcrypt.compare(secretcode, dbrecord.secretcode)){
+                console.log(`Login successful for user ${dbrecord.username}!`);
+                res.send(`<h1>Hello ${username}!</h1><br><br><br><a href="index.html">log out</a>`);
+            } else {
+                res.status(427).send(`<h1 style="color:red;">Invalid password/secretcode!</h1><br><a href="login.html">Return to Login page</a>`);
+            }
+        } else {
+            res.status(427).send(`<h1 style="color:red;">Invalid User Name!</h1><br><a href="login.html">Return to Login page</a>`);
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(`<h1 style="color:red;">Registration failed!</h1><br><a href="http://localhost:${PORT}/">Return to website</a>`);
+    }
+
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
